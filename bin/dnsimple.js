04c59afb8712ce918ea32671caec2e4561f082a3
Adam Anderly <adam@anderly.com>
2014-11-03 08:55:56 -0600

    Changes to support Sandbox environment.

--- bin/dnsimple.js
+++ bin/dnsimple.js
@@ -1,22 +1,24 @@
-// 
-// Copyright (c) Microsoft and contributors.  All rights reserved.
-// 
+//
+// Copyright (c) Adam Anderly.  All rights reserved.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
+//
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// NOTICE: Based on azure-cli (https://github.com/Azure/azure-sdk-tools-xplat/)
 // 
 
-var AzureCli = require('../lib/cli');
+var DnsimpleCli = require('../lib/cli');
 
-var cli = new AzureCli();
+var cli = new DnsimpleCli();
 cli.parse(process.argv);
 if (cli.args.length === 0) {
   cli.parse(['', '', '-h']);
